7e1f8114e3de8e02041e2bcbbd184b05f1d4b758
Jonathan Reem <jonathan.reem@gmail.com>
2014-11-17 20:12:00 -0800

    (feat) Add an Operation trait.

--- src/lib.rs
+++ src/lib.rs
@@ -10,3 +10,20 @@ pub trait Callback<T, O> {
     fn invoke(self, T) -> O;
 }
 
+// All unboxed closures are callbacks.
+impl<I, O, F: FnOnce(I) -> O> Callback<I, O> for F {
+    fn invoke(self, val: I) -> O {
+        self.call_once((val,))
+    }
+}
+
+/// An asynchronous operation, executable with a callback.
+pub trait Operation<T> {
+    /// Execute this operation, feeding its result to the passed-in callback.
+    fn execute<Co, C: Callback<T, Co>>(self, C);
+}
+
+// Actually implementing the above trait for any kind of
+// fn or closure requires higher-ranked types, so no
+// blanket implementations for now.
+
